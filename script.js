1092a5ea9976ddbf167a6482ee9930d60123627e
Rahulllkr <rahulkumar8604745@gmail.com>
2025-07-08 23:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,31 +3,25 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-	const priceElement = document.querySelectorAll(".prices")
+	const priceElements = document.querySelectorAll(".price");
 	let total = 0;
 
-	priceElement.forEach((ele) => {
+	priceElements.forEach((ele) => {
 		total += Number(ele.textContent);
 	});
+
 	const newRow = document.createElement("tr");
 	const newCell = document.createElement("td");
 	newCell.colSpan = 2;
-	// newCell.style.fontWeight = "bold";
-    // newCell.style.textAlign = "center";
-	newCell.textContent = `Total Price: ${total}`
+	newCell.textContent = `Total Price: ${total}`;
 
 	newRow.appendChild(newCell);
 
 	const table = document.querySelector("table");
 	table.appendChild(newRow);
 
+	// Correct property to disable
 	getSumBtn.disabled = true;
-
-
-	
-  
 };
 
 getSumBtn.addEventListener("click", getSum);
-
